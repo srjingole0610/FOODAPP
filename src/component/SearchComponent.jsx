e9b7c3584e2ebb75608d0dddc984bd2c3b16f19f
Suraj Ingole <74641861+srjingole0610@users.noreply.github.com>
2024-10-18 20:19:22 +0530

    Added functionality to make the api call

--- src/component/SearchComponent.jsx
+++ src/component/SearchComponent.jsx
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from 'react'
 
 function SearchComponent() {
-    const [query, setQuery] = useState("")
+    const [query, setQuery] = useState("pizza")
+    const URL = "https://api.spoonacular.com/recipes/complexSearch"
+    const API_KEY = "b1b6bd6fae0642bb93dcf4fd947f2ec8"
 
     useEffect(() => {
-        const fetchDemo = () => {
-            console.log("demo function executed");
+        const fetchFood = async () => {
+            const response = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`)
+            const parsedResponse = await response.json()
         }
-        fetchDemo();
-    }, [])
+        fetchFood();
+    }, [query])
 
     const handleInputChange = (event) => {
         setQuery(event.target.value)
